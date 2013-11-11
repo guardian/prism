d77f4cb9e063ada2a44634808f7f7690768bc624
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-11-11 18:37:57 +0000

    Migrate over to commander properly

--- lib/marauder/commands.rb
+++ lib/marauder/commands.rb
@@ -16,53 +16,18 @@ API_KEY='8TQMOsrLKeXhWaLmULvtj7wk7AZvtTxv' # 'marauder' key
 MAX_SSH_HOSTS = 4
 DEFAULT_USER = 'jetty'
 
+# def usage
+#   puts "usage: marauder <filter>"
+#   puts "       marauder list <filter>"
+#   puts "       marauder hosts <filter>"
+#   puts "       marauder ssh <filter> -- <command>"
+# end
 
-def usage
-  puts "usage: marauder <filter>"
-  puts "       marauder list <filter>"
-  puts "       marauder hosts <filter>"
-  puts "       marauder ssh <filter> -- <command>"
-end
+# if ARGV.empty?
+#   usage
+#   exit
+# end
 
-
-if ARGV.empty?
-  usage
-  exit
-end
-
-action, *remaining =
-  case ARGV[0]
-  when 'ssh', 'list', 'hosts'
-    ARGV
-  else # default
-    ['list', *ARGV]
-  end
-
-query, command =
-  if remaining.include?('--')
-    [remaining.take_while {|s| s != '--'},
-     remaining.drop_while {|s| s != '--'}.drop(1).join(' ')]
-  else
-    [remaining]
-  end
-
-query.map!(&:downcase).map! {|s| Regexp.new("^#{s}.*")}
-
-# TODO: allow customising the user, maybe 'ssh --user <foo> ...'
-user = DEFAULT_USER
-
-
-data = HTTParty.get(API, :query => {:key => API_KEY})
-response = data["response"]
-
-stale = response["stale"]
-update_time = response["updateTime"]
-
-if stale
-  STDERR.puts "WARNING: Riff-Raff reports that this deployinfo is stale, it was last updated at #{update_time}"
-end
-
-hosts = response["results"]["hosts"]
 
 def tokenize(s)
   separators = ['-', '_', '::']
@@ -76,43 +41,90 @@ def confirm
   STDIN.gets.chomp == 'yes'
 end
 
-matching = hosts.select do |host|
-  query.all? do |name|
-    tokens = tokenize(host["app"]) + [host["app"], host["stage"]]
-    tokens.any? {|token| name.match(token.downcase)}
+def find_hosts(filter)
+  query = filter.map(&:downcase).map{|s| Regexp.new("^#{s}.*")}
+
+  data = HTTParty.get(API, :query => {:key => API_KEY})
+  response = data["response"]
+
+  stale = response["stale"]
+  update_time = response["updateTime"]
+
+  if stale
+    STDERR.puts "WARNING: Riff-Raff reports that this deployinfo is stale, it was last updated at #{update_time}"
+  end
+
+  hosts = response["results"]["hosts"]
+
+  hosts.select do |host|
+    query.all? do |name|
+      tokens = tokenize(host["app"]) + [host["app"], host["stage"]]
+      tokens.any? {|token| name.match(token.downcase)}
+    end
   end
 end
 
-case action
-when 'list'
-  matching.each do |host|
-    puts "#{host['stage']}\t#{host['app']}\t#{host['hostname']}\t#{host['created_at']}"
+
+command :list do |c|
+  c.description = 'Display list of hosts' 
+  c.option '--test STRING', String, 'Test a parameter'
+  c.action do |args, options|
+    matching = find_hosts(args)
+    matching.each do |host|
+      puts "#{host['stage']}\t#{host['app']}\t#{host['hostname']}\t#{host['created_at']}"
+    end
   end
+end
 
-when 'hosts'
-  matching.each do |host|
-    puts host['hostname']
+command :hosts do |c|
+  c.description = 'Display only hostnames' 
+  c.action do |args, options|
+    matching = find_hosts(args)
+    matching.each do |host|
+      puts host['hostname']
+    end
   end
+end
 
-when 'ssh'
-  if command.nil?
-    puts "Please provide a command."
-    usage
-    exit 1
-  else if matching.size > MAX_SSH_HOSTS
-    puts "Do you really want to SSH into #{matching.size} hosts?"
-    exit 1 unless confirm
-  end
+command :ssh do |c|
+  c.syntax = 'marauder ssh <filter> -- <command>'
+  c.description = 'Execute command on matching hosts' 
+  c.option '--user STRING', String, 'Remote username'
+  c.option '--cmd STRING', String, 'Command to execute'
+  c.action do |args, options|
+    options.default :user => DEFAULT_USER
 
-  puts "ssh into #{matching.size} hosts and run `#{command}`..."
-  puts
+    STDERR.puts "#{args}"
 
-  matching.each do |host|
-    Net::SSH.start(host['hostname'], user) do |ssh|
-      puts "== #{host['hostname']} =="
-      puts ssh.exec!(command)
-      puts
+    query = args.take_while {|s| s != '--'}
+    cmd = options.cmd
+
+    STDERR.puts "Query: #{query}"
+    STDERR.puts "Command: #{cmd}"
+
+    matching = find_hosts(query)
+
+    if cmd.nil?
+      puts "Please provide a command."
+      usage
+      exit 1
+    else if matching.size > MAX_SSH_HOSTS
+      puts "Do you really want to SSH into #{matching.size} hosts?"
+      exit 1 unless confirm
+    end
+
+    puts "ssh into #{matching.size} hosts and run `#{cmd}`..."
+    puts
+
+    matching.each do |host|
+      Net::SSH.start(host['hostname'], options.user) do |ssh|
+        puts "== #{host['hostname']} =="
+        puts ssh.exec!(cmd)
+        puts
+        end
       end
     end
   end
 end
+
+default_command :list
