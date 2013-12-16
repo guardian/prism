358caaad6e2722f1156ce81c193edb33b25f581b
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-12-16 12:37:00 +0000

    Include plugin that doesn't override sane META-INF behaviour

--- project/project/build.scala
+++ project/project/build.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object DeployPlugins extends Build {
-  val playArtifactPluginVersion = "play2.2.0_2"
+  val playArtifactPluginVersion = "play2.2.0_3"
 
   lazy val plugins = Project("plugins", file("."))
     .dependsOn(uri("git://github.com/guardian/sbt-play-artifact.git#" + playArtifactPluginVersion)
