44c6001f9c5dade247a062eea20e8116d13bbc18
Mario Galic <mario.galic@guardian.co.uk>
2019-12-18 09:43:16 +0000

    How to inject routes at compile time to get at routes.documentation?

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,14 +2,12 @@ package controllers
 
 import play.api.Configuration
 import play.api.mvc._
-import router.Routes
 
-//class Application(routes: Routes, configuration: Configuration) extends Controller {
 class Application(configuration: Configuration) extends Controller {
   def index = Action {
     // FIXME: How to inject routes at compile time to get at routes.documentation?
-//    Ok(views.html.index(routes.documentation))
-    Ok("woohoo")
+    // Ok(views.html.index(routes.documentation))
+    Ok("ok")
   }
 
   def config = Action {
