85f60cccefad801c7f2ab70e6efaff35e3b52702
Mario Galic <mario.galic@guardian.co.uk>
2019-12-18 13:35:29 +0000

    router parameter is by-name because otherwise StackOverflowError is thrown due to initialisation order problem in ApplicationLoader

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api.Configuration
 import play.api.mvc._
 import play.api.routing.Router
 
+// router parameter is by-name because otherwise StackOverflowError is thrown due to initialisation order problem in ApplicationLoader
 class Application(configuration: Configuration, router: => Router) extends Controller {
   def index = Action {
     Ok(views.html.index(router.documentation))
