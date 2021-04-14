5fcfb1f80a45749dbae4c69be246215e7df7d6a2
Akash Askoolum <akash1810@gmail.com>
2021-04-14 10:43:55 +0100

    fix: Keep the logicalId of the, stateful, load balancer
    It looks like we were relying on odd behaviour before.
    
    See:
      - https://github.com/guardian/cdk/pull/440
      - https://github.com/guardian/cdk/commit/0d0eba739e97c18baa411f3f4eb58b6fa26727bb#diff-ddcd012c9ad844faf4c3b865a658f5e3e600d62ad88228ca022798def7c56c9dL33

--- cdk/lib/prism.ts
+++ cdk/lib/prism.ts
@@ -114,6 +114,7 @@ export class PrismStack extends GuStack {
       listener: {
         allowConnectionsFrom: [Peer.ipv4("10.0.0.0/8")],
       },
+      existingLogicalId: "LoadBalancer",
     });
 
     appServerSecurityGroup.connections.allowFrom(loadBalancer, Port.tcp(9000), "Port 9000 LB to fleet");
