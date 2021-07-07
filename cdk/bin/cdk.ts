d10823a1bd76060db88f6c9d11d77f32630cce4a
Jacob Winch <jacob.winch@guardian.co.uk>
2021-07-07 11:40:14 +0100

    Remove old version of stack (which uses constructs)

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,17 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import { App } from "@aws-cdk/core";
-import { PrismStack } from "../lib/prism";
 import { PrismAccess } from "../lib/prism-access";
 import { PrismEc2App } from "../lib/prism-ec2-app";
 
 const app = new App();
-
-new PrismStack(app, "Prism", {
-  description: "Prism - service discovery",
-  migratedFromCloudFormation: true,
-  stack: "deploy",
-});
 
 new PrismEc2App(app, "PrismEc2App", {
   description: "Prism - service discovery",
