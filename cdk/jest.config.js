67f04be7c87d5affcf612bf3604acb555d4de2d9
Akash Askoolum <akash1810@gmail.com>
2022-11-08 07:54:45 +0000

    chore(deps): Update Jest to v29

--- cdk/jest.config.js
+++ cdk/jest.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  testMatch: ['<rootDir>/lib/**/*.test.ts'],
+  testMatch: ["<rootDir>/lib/**/*.test.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "ts-jest",
   },
   setupFilesAfterEnv: ["./jest.setup.js"],
+
+  // Preserve snapshot format during initial Jest 29 upgrade.
+  // See https://jestjs.io/docs/upgrading-to-jest29.
+  // TODO remove this!
+  snapshotFormat: {
+    escapeString: true,
+    printBasicPrototype: true,
+  },
 };
